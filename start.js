408204d548bf98a7182180e0711f57e1761166d0
Donald P Polansky <44909626+Zizuar@users.noreply.github.com>
2025-06-17 16:56:52 -0500

    Update start.js

--- start.js
+++ start.js
@@ -10,8 +10,8 @@
      app.prepare().then(() => {
        createServer((req, res) => {
          handle(req, res);
-       }).listen(3000, (err) => {
+       }).listen(3001, (err) => {
          if (err) throw err;
-         console.log('> Ready on http://localhost:3000');
+         console.log('> Ready on http://localhost:3001');
        });
      });
